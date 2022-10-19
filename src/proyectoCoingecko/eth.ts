18753f2d3a110f7afe5bfe7d045316dbe1135a98
Calcifer1001 <dseidlertoken@gmail.com>
2022-10-19 19:46:19 -0300

    [ADD] readme

--- src/proyectoCoingecko/eth.ts
+++ src/proyectoCoingecko/eth.ts
@@ -2,3 +2,4 @@ import { getPrice } from '../coingecko'
 
 console.log("Hello world. This is Kenrou IT. Conflict generated and resolved")
 console.log("I'm so cool creating branches")
+console.log("I'm so cool creating branches")
